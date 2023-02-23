853505179dc23ef449aa44c836be1617855406db
mateorogatky <mateorogatky@gmail.com>
2023-02-23 18:10:17 -0300

    primer commit

--- babyControl/qrApp/src/app/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { RootState, AppDispatch } from "./store";
-//Hook que permite actualizar de forma "automatica" el tipo de dato de los distintos rootState
-//HOOK QUE SE USAN EN REMPLAZO DE useDispatch y useSelector
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
